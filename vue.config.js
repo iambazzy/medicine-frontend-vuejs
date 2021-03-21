3e027de8ab98d7831fcfd2525b43cc3755d26875
iambazzy <basittammy@gmail.com>
2021-03-21 20:33:12 +0530

    some hnge

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,9 @@
-// const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = {
   transpileDependencies: [
     'vuetify'
   ],
   configureWebpack: {
-    // plugins: [ new BundleAnalyzerPlugin() ],
     optimization: {
       minimize: true,
       splitChunks: {
